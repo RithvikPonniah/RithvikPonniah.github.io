05ce6a3f4aaf65830bcc6872976d75609c8b4d0b
RithvikPonniah <122193883+RithvikPonniah@users.noreply.github.com>
2023-01-11 20:23:02 -0500

    made changes to make latest update appear on the top

--- 20Apps/.idea/numbers.py
+++ 20Apps/.idea/numbers.py
@@ -3,5 +3,5 @@ number2 = 1
 numbers = []
 while number2 <= number :
     numbers.append(number2)
-    print(join(numbers) )
+    print(numbers )
     number2 = number2+1
---
+++ 20Apps/.idea/numbers2.py
@@ -0,0 +1,7 @@
+number = int(input("enter input"))
+number2 = 1
+numbers = ""
+while number2 <= number :
+    numbers = numbers +str(number2) + ","
+    print(numbers)
+    number2 = number2 + 1
