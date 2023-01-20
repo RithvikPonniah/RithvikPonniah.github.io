b542eec476fbcb665e9c76fb12943931c23de5a8
RithvikPonniah <122193883+RithvikPonniah@users.noreply.github.com>
2023-01-19 22:06:50 -0500

    learnings

--- 20Apps/excerise.py
+++ 20Apps/excerise.py
@@ -1,4 +1,12 @@
-friends = ["karthik","Arush","Sidharth","shreyas"]
-print(friends[2])
-friends[2] = "Sidhu"
-print(friends[2])
+dollars = int(input("how many dollars have you got: "))
+euros = dollars * 0.95
+print("you have got " , euros , "euros")
+
+rank = ["john","sen","lisa"]
+name = int(input("type the rank you want to knowout of 1, 2 or 3 "))
+print(rank[name-1])
+
+rank = ['John', 'Sen', 'Lisa']
+name = input("Enter a name: ").capitalize()
+rank1 = rank.index(name) + 1
+print(rank1)
