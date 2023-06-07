bbc909bdf71a9a8429f20fbb867f8e4a1f5a15a8
RithvikPonniah <69447390+ponniahRajagopal@users.noreply.github.com>
2023-06-06 22:10:42 -0400

    learnings

--- 20Apps/gui.py
+++ 20Apps/gui.py
@@ -1,10 +1,12 @@
 import todosModule
 import PySimpleGUI as sg
 
-label = sg.Text("Type in a todo")
-input = sg.InputText(tooltip="Useless fellow" , key = "todo")
-add = sg.Button("Add")
-
+label       = sg.Text("Type in a todo")
+input       = sg.InputText(tooltip="Useless fellow" , key = "todo")
+add         = sg.Button("Add")
+list_box    = sg.Listbox(values=todosModule.getTodo(),key='todos',
+                      enable_events=True,size=[45, 10])
+edit = sg.Button("Edit")
 win = sg.Window("My first window",
                 layout=[[label],[input,add]],
                 font=("Helvetica",20))
@@ -15,7 +17,18 @@ while True :
     match event :
         case "Add":
             todos = todosModule.getTodo()
-            new_todo = values['todo']
+            print (todos)
+            new_todo = values['todo'] + '\n'
             todos.append(new_todo)
+            print (todos)
             todosModule.addTodo(todos)
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+            todos = todosModule.getTodo()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            todosModule.addTodo(todos)
+        case sg.WIN_CLOSED:
+            break
 win.close()
--- 20Apps/todosModule.py
+++ 20Apps/todosModule.py
@@ -1,51 +1,62 @@
-def writeToFile(todos,filePath='files/todos.txt'):
+def writeToFile(todos, filePath='files/todos.txt'):
     with open(filePath, 'w') as fObject:
         fObject.writelines(todos)
 
+
 def getTodo(filePath='files/todos.txt'):
     with open(filePath, 'r') as fObject:
-        dodo =  fObject.readlines()
+        dodo = fObject.readlines()
     return dodo
 
-def addTodo(todoItem,filePath='files/todos.txt'):
-    with open(filePath, 'a') as fObject:
+
+def addTodo(todoItem, filePath='files/todos.txt'):
+    with open(filePath, 'w') as fObject:
         fObject.writelines(todoItem)
-def editTodo(todoNumber,filePath='files/todos.txt'):
+
+
+def editTodo(todoNumber, filePath='files/todos.txt'):
     try:
         todos = getTodo()
         x = input("Enter the new value of the todo items: ") + '\n'
-        z = todos[ todoNumber- 1]
+        z = todos[todoNumber - 1]
         todos[todoNumber - 1] = x
         writeToFile(todos)
         z = z.strip('\n')
         x = x.strip('\n')
         print(f"You have changed '{z}' into '{x}'")
-    except IndexError :
-        print("The given todo number " ,todoNumber,'does not exist')
-def completeTodo(todoNumber,filePath='files/todos.txt'):
-    try :
+    except IndexError:
+        print("The given todo number ", todoNumber, 'does not exist')
+
+
+def completeTodo(todoNumber, filePath='files/todos.txt'):
+    try:
         todos = getTodo(filePath)
         todos.pop(todoNumber - 1)
-        writeToFile(todos,filePath)
+        writeToFile(todos, filePath)
         for index, item in enumerate(todos):
             item = item.strip('\n')
             row = f"{index + 1}.{item}"
             print(row)
     except IndexError:
         print('The Index you have given is invalid')
-def showTodo(filePath='files/todos.txt') :
+
+
+def showTodo(filePath='files/todos.txt'):
     todos = getTodo(filePath)
     for index, item in enumerate(todos):
         # x = x + 1
         item = item.strip('\n')
         row = f"{index + 1}.{item}"
         print(row)
+
+
 def clearTodo(filePath='files/todos.txt'):
     clear = input("Are you sure you want to clear all your todos ? press  'y' to confirm ").strip()
     if clear == "y":
         todos = []
-        writeToFile(todos,filePath)
+        writeToFile(todos, filePath)
 
-if __name__ == '__main__' :
+
+if __name__ == '__main__':
     print(__name__)
-    print ("Burgundi")
+    print("Burgundi")
